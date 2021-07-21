2bcd077959a0575bb33dfed4e6339e6338df7ae7
Mi-Gyeong <rmfosem613@gmail.com>
2021-07-21 18:37:39 +0900

    init

---
+++ TMDB_API/client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = 'e1d5657438192648dca986a759fc9c6a';
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
