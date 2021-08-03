28cddcf4c064ea1b9061cb3af3053c8f882d6249
noion0511 <noion0511@gmail.com>
2021-08-03 13:05:57 +0900

    d

--- react/src/components/Carousel/Carousel.js
+++ react/src/components/Carousel/Carousel.js
@@ -36,7 +36,7 @@ const Gallery = ({ id, media_type }) => {
 
   const fetchCredits = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+      `https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=e1d5657438192648dca986a759fc9c6a&language=en-US`
     );
     setCredits(data.cast);
   };
