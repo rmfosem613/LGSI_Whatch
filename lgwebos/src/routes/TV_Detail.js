a524b9510986ea124a6ec4b3120fd07d4abcf588
kkimhaji <kkim.haji@gmail.com>
2021-08-04 13:12:58 +0900

    tv details 수정

--- lgwebos/src/routes/TV_Detail.js
+++ lgwebos/src/routes/TV_Detail.js
@@ -28,7 +28,7 @@ class Detail extends React.Component {
                     </td>
                     <td>
                         <div className="movie__title">
-                            <h3>{location.state.name}</h3>
+                            <h2>{location.state.name}</h2>
                             <ul className="movie__vote_average">
                                 <img className="star" src="https://image.flaticon.com/icons/png/512/2107/2107957.png"></img>
                                 {location.state.vote_average}
